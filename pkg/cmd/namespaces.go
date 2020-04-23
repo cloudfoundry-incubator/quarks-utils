67ba786c3b5242930818c31021cd482973bb2dfc
Mario Manno <mario.manno@suse.com>
2020-04-23 13:12:41 +0200

    Add monitored id to shared config

--- pkg/cmd/namespaces.go
+++ pkg/cmd/namespaces.go
@@ -19,13 +19,6 @@ func OperatorNamespace(cfg *config.Config, log *zap.SugaredLogger, name string) 
 	return operatorNamespace
 }
 
-// WatchNamespace sets the namespace which is watched and where most resources are created
-func WatchNamespace(cfg *config.Config, log *zap.SugaredLogger) string {
-	watchNamespace := viper.GetString("watch-namespace")
-	cfg.Namespace = watchNamespace
-	return watchNamespace
-}
-
 // OperatorNamespaceFlags adds to viper flags
 func OperatorNamespaceFlags(pf *flag.FlagSet, argToEnv map[string]string, name string) {
 	pf.StringP(name, "n", "default", "The operator namespace, for the webhook service")
@@ -33,15 +26,6 @@ func OperatorNamespaceFlags(pf *flag.FlagSet, argToEnv map[string]string, name s
 	viper.BindPFlag(name, pf.Lookup(name))
 
 	argToEnv[name] = envName(name)
-}
-
-// WatchNamespaceFlags adds to viper flags
-func WatchNamespaceFlags(pf *flag.FlagSet, argToEnv map[string]string) {
-	pf.StringP("watch-namespace", "a", "staging", "Act on this namespace, watch for BOSH deployments and create resources")
-
-	viper.BindPFlag("watch-namespace", pf.Lookup("watch-namespace"))
-
-	argToEnv["watch-namespace"] = "WATCH_NAMESPACE"
 }
 
 func envName(name string) string {
