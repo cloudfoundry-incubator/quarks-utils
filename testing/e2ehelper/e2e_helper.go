ada675f151cf456803bd968d45c52edd13587c3b
Mario Manno <mario.manno@suse.com>
2020-04-28 16:41:24 +0200

    Shared e2e helpers for namespace and chart install
    The 'SetUpEnvironment' was split into create namespace and install
    chart.

--- testing/e2ehelper/e2e_helper.go
+++ testing/e2ehelper/e2e_helper.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/onsi/ginkgo/config"
@@ -15,12 +14,11 @@ import (
 
 const (
 	installTimeOutInSecs = "600"
-	e2eFailedMessage     = "e2e test setting up environment failed."
+	e2eFailedMessage     = "Failed setting up e2e test environment."
 )
 
 var (
-	nsIndex   int
-	namespace string
+	nsIndex int
 )
 
 // TearDownFunc tears down the resource
@@ -43,63 +41,19 @@ func TearDownAll(funcs []TearDownFunc) error {
 	return nil
 }
 
-// SetUpEnvironment ensures helm binary can run
-// being able to reach tiller, and eventually it
-// will install the cf-operator chart.
-func SetUpEnvironment(chartPath string) (string, string, TearDownFunc, error) {
-	err := testing.RunHelmBinaryWithCustomErr("version")
-	if err != nil {
-		switch err := err.(type) {
-		case *testing.CustomError:
-			if strings.Contains(err.StdOut, "could not find tiller") {
-				return "", "", nil, errors.Wrapf(err, "%s Helm v2 needs to be installed with RBAC first.", e2eFailedMessage)
-			}
-		default:
-			return "", "", nil, errors.Wrapf(err, "%s Helm version command failed", e2eFailedMessage)
-		}
-	}
-
-	operatorNamespace, err := CreateTestNamespace()
-	namespace = fmt.Sprintf("%s-work", operatorNamespace)
+// CreateNamespace creates the operator namespace and returns the generated single namespace name
+func CreateNamespace() (string, string, TearDownFunc, error) {
+	operatorNamespace, err := createTestNamespace()
 	if err != nil {
 		return "", "", nil, errors.Wrapf(err, "%s Creating test namespace failed.", e2eFailedMessage)
 	}
-	fmt.Println("Setting up in test namespace '" + namespace + "'...")
 
-	helmVersion, err := testing.HelmBinaryVersion()
-	if err != nil {
-		return "", "", nil, errors.Wrapf(err, "%s Helm version command failed.", e2eFailedMessage)
-	}
+	namespace := fmt.Sprintf("%s-work", operatorNamespace)
 
-	err = installHelmChart(helmVersion, operatorNamespace, namespace, chartPath)
-	if err != nil {
-		return "", "", nil, errors.Wrapf(err, "%s Helm install command failed.", e2eFailedMessage)
-	}
-
-	// Add sleep for workaround for CI timeouts
-	time.Sleep(10 * time.Second)
+	fmt.Printf("Using test namespace: %s, %s\n", operatorNamespace, namespace)
 
 	teardownFunc := func() error {
 		var messages string
-		err = testing.DeleteSecret(namespace, "cf-operator-webhook-server-cert")
-		if err != nil {
-			messages = fmt.Sprintf("%v%v\n", messages, err.Error())
-		}
-
-		err = testing.DeleteWebhooks(namespace)
-		if err != nil {
-			messages = fmt.Sprintf("%v%v\n", messages, err.Error())
-		}
-
-		if helmVersion == "2" {
-			err = testing.RunHelmBinaryWithCustomErr("delete", fmt.Sprintf("%s-%s", testing.CFOperatorRelease, operatorNamespace), "--purge")
-		} else {
-			err = testing.RunHelmBinaryWithCustomErr("delete", "-n", operatorNamespace, fmt.Sprintf("%s-%s", testing.CFOperatorRelease, operatorNamespace))
-		}
-		if err != nil {
-			messages = fmt.Sprintf("%v%v\n", messages, err.Error())
-		}
-
 		err = testing.DeleteNamespace(namespace)
 		if err != nil {
 			messages = fmt.Sprintf("%v%v\n", messages, err.Error())
@@ -111,18 +65,17 @@ func SetUpEnvironment(chartPath string) (string, string, TearDownFunc, error) {
 		}
 
 		if messages != "" {
-			fmt.Printf("Failures while cleaning up test environment for '%s':\n %v", namespace, messages)
+			fmt.Printf("Failures while cleaning up test environment namespaces:\n %v", messages)
 			return errors.New(messages)
 		}
-		fmt.Println("Cleaned up test environment for '" + namespace + "'")
 		return nil
 	}
 
 	return namespace, operatorNamespace, teardownFunc, nil
 }
 
-// CreateTestNamespace generates a namespace based on a env variable
-func CreateTestNamespace() (string, error) {
+// createTestNamespace generates a namespace based on a env variable
+func createTestNamespace() (string, error) {
 	prefix, found := os.LookupEnv("TEST_NAMESPACE")
 	if !found {
 		prefix = "default"
@@ -138,24 +91,36 @@ func CreateTestNamespace() (string, error) {
 	return namespace, nil
 }
 
-func installHelmChart(helmVersion, operatorNamespace, namespace, chartPath string) error {
-	var err error
-	if helmVersion == "2" {
-		err = testing.RunHelmBinaryWithCustomErr("install", chartPath,
-			"--name", fmt.Sprintf("%s-%s", testing.CFOperatorRelease, operatorNamespace),
-			"--namespace", operatorNamespace,
-			"--timeout", installTimeOutInSecs,
-			"--set", fmt.Sprintf("global.operator.watchNamespace=%s", namespace),
-			"--wait")
-	} else {
-		err = testing.RunHelmBinaryWithCustomErr("install",
-			fmt.Sprintf("%s-%s", testing.CFOperatorRelease, operatorNamespace),
-			chartPath,
-			"--namespace", operatorNamespace,
-			"--timeout", fmt.Sprintf("%ss", installTimeOutInSecs),
-			"--set", fmt.Sprintf("global.operator.watchNamespace=%s", namespace),
-			"--wait")
-
+// InstallChart installs the helm chart into the operator namespace
+func InstallChart(chartPath string, operatorNamespace string, args ...string) (TearDownFunc, error) {
+	err := testing.RunHelmBinaryWithCustomErr("version")
+	if err != nil {
+		return nil, errors.Wrapf(err, "%s Helm version command failed", e2eFailedMessage)
 	}
-	return err
+
+	cmd := append([]string{
+		"install",
+		fmt.Sprintf("%s-%s", testing.CFOperatorRelease, operatorNamespace),
+		chartPath,
+		"--namespace", operatorNamespace,
+		"--timeout", fmt.Sprintf("%ss", installTimeOutInSecs),
+		"--wait",
+	}, args...)
+
+	err = testing.RunHelmBinaryWithCustomErr(cmd...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "%s Helm install command failed.", e2eFailedMessage)
+	}
+
+	// Add sleep for workaround for CI timeouts
+	time.Sleep(10 * time.Second)
+
+	return func() error {
+		err = testing.RunHelmBinaryWithCustomErr("delete", "-n", operatorNamespace, fmt.Sprintf("%s-%s", testing.CFOperatorRelease, operatorNamespace))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, nil
 }
