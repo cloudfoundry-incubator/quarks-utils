bc368dd626a3c70ba63a6bba5bd87198b3e4f510
Ettore Di Giacinto <edigiacinto@suse.com>
2020-04-30 08:02:31 +0200

    Refactor tests to follow the project style
    Address to review comments

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -12,33 +12,35 @@ import (
 
 var _ = Describe("Shared config", func() {
 	Describe("constructors", func() {
+
 		It("sets only meltdown defaults from constants, without manipulating arguments", func() {
 			for _, fs := range []afero.Fs{nil, afero.NewOsFs()} {
-				sharedConfig := config.NewDefaultConfig(fs)
-				Expect(sharedConfig.Namespace).To(BeEmpty())
-				Expect(sharedConfig.OperatorNamespace).To(BeEmpty())
-				Expect(sharedConfig.WebhookServerHost).To(BeEmpty())
-				Expect(sharedConfig.CtxTimeOut).To(Equal(0 * time.Second))
-				Expect(sharedConfig.WebhookUseServiceRef).To(BeFalse())
-				Expect(sharedConfig.WebhookServerPort).To(Equal(int32(0)))
+				c := config.NewDefaultConfig(fs)
+				Expect(c.Namespace).To(BeEmpty())
+				Expect(c.OperatorNamespace).To(BeEmpty())
+				Expect(c.WebhookServerHost).To(BeEmpty())
+				Expect(c.CtxTimeOut).To(Equal(0 * time.Second))
+				Expect(c.WebhookUseServiceRef).To(BeFalse())
+				Expect(c.WebhookServerPort).To(Equal(int32(0)))
 				// Check there is no manipulation with fs
 				if fs == nil {
-					Expect(sharedConfig.Fs).To(BeNil())
+					Expect(c.Fs).To(BeNil())
 				} else {
-					Expect(sharedConfig.Fs).To(Equal(fs))
+					Expect(c.Fs).To(Equal(fs))
 				}
-				Expect(sharedConfig.MaxBoshDeploymentWorkers).To(Equal(0))
-				Expect(sharedConfig.MaxQuarksJobWorkers).To(Equal(0))
-				Expect(sharedConfig.MaxQuarksSecretWorkers).To(Equal(0))
-				Expect(sharedConfig.MaxQuarksStatefulSetWorkers).To(Equal(0))
-				Expect(sharedConfig.MeltdownDuration).To(Equal(config.MeltdownDuration))
-				Expect(sharedConfig.MeltdownRequeueAfter).To(Equal(config.MeltdownRequeueAfter))
+				Expect(c.MaxBoshDeploymentWorkers).To(Equal(0))
+				Expect(c.MaxQuarksJobWorkers).To(Equal(0))
+				Expect(c.MaxQuarksSecretWorkers).To(Equal(0))
+				Expect(c.MaxQuarksStatefulSetWorkers).To(Equal(0))
+				Expect(c.MeltdownDuration).To(Equal(config.MeltdownDuration))
+				Expect(c.MeltdownRequeueAfter).To(Equal(config.MeltdownRequeueAfter))
 			}
 		})
+
 		It("properly sets a shared config", func() {
 			fs := afero.NewOsFs()
 
-			sharedConfig := config.NewConfig(
+			c := config.NewConfig(
 				"test",
 				"operator-namespace",
 				10,
@@ -48,19 +50,20 @@ var _ = Describe("Shared config", func() {
 				fs,
 				1, 2, 3, 4, 5, 6)
 
-			Expect(sharedConfig.Namespace).To(Equal("test"))
-			Expect(sharedConfig.OperatorNamespace).To(Equal("operator-namespace"))
-			Expect(sharedConfig.WebhookServerHost).To(Equal("0.0.0.0"))
-			Expect(sharedConfig.CtxTimeOut).To(Equal(10 * time.Second))
-			Expect(sharedConfig.WebhookUseServiceRef).To(BeTrue())
-			Expect(sharedConfig.WebhookServerPort).To(Equal(int32(8080)))
-			Expect(sharedConfig.Fs).To(Equal(fs))
-			Expect(sharedConfig.MaxBoshDeploymentWorkers).To(Equal(1))
-			Expect(sharedConfig.MaxQuarksJobWorkers).To(Equal(2))
-			Expect(sharedConfig.MaxQuarksSecretWorkers).To(Equal(3))
-			Expect(sharedConfig.MaxQuarksStatefulSetWorkers).To(Equal(4))
-			Expect(sharedConfig.MeltdownDuration).To(Equal(5 * time.Second))
-			Expect(sharedConfig.MeltdownRequeueAfter).To(Equal(6 * time.Second))
+			Expect(c.Namespace).To(Equal("test"))
+			Expect(c.OperatorNamespace).To(Equal("operator-namespace"))
+			Expect(c.WebhookServerHost).To(Equal("0.0.0.0"))
+			Expect(c.CtxTimeOut).To(Equal(10 * time.Second))
+			Expect(c.WebhookUseServiceRef).To(BeTrue())
+			Expect(c.WebhookServerPort).To(Equal(int32(8080)))
+			Expect(c.Fs).To(Equal(fs))
+			Expect(c.MaxBoshDeploymentWorkers).To(Equal(1))
+			Expect(c.MaxQuarksJobWorkers).To(Equal(2))
+			Expect(c.MaxQuarksSecretWorkers).To(Equal(3))
+			Expect(c.MaxQuarksStatefulSetWorkers).To(Equal(4))
+			Expect(c.MeltdownDuration).To(Equal(5 * time.Second))
+			Expect(c.MeltdownRequeueAfter).To(Equal(6 * time.Second))
 		})
+
 	})
 })
