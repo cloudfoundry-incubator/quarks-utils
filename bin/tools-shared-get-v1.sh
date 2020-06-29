b983db9e23c2fd2fc3f1cf43a10ff979f0a03269
Mario Manno <mario.manno@suse.com>
2020-06-29 15:35:26 +0200

    Add bin/tools-shared-get-v1.sh to download the shared bin dir

---
+++ bin/tools-shared-get-v1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+get_quarks_utils_bin() {
+  GIT_ROOT=${GIT_ROOT:-$(git rev-parse --show-toplevel)}
+  cd $GIT_ROOT
+
+  # Gets the quarks-utils version from go.mod
+  quarks_utils_version=$(grep code.cloudfoundry.org/quarks-utils go.mod | awk '{print $2}')
+  quarks_utils_version=$(echo $quarks_utils_version | sed 's/.*-//g') # Keep only the commit
+  if [ "$quarks_utils_version" = "utils" ]; then
+    echo "failed to parse commit from go.mod"
+    exit 1
+  fi
+
+  mkdir -p tools/quarks-utils
+  wget -qO - https://github.com/cloudfoundry-incubator/quarks-utils/archive/"$quarks_utils_version".tar.gz | \
+    tar -C tools/quarks-utils -xz --strip-components=1 --no-anchored bin/
+}
+
+get_quarks_utils_bin
